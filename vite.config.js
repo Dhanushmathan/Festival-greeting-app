7c52ba44587ce8785c0affb55b0a59c0eb15db99
Dhanushmathan <lovelymathan2394@gmail.com>
2024-12-09 16:16:52 +0530

    modifying vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     base: '/Festival-greeting-app/',
     build: {
         rollupOptions: {
             input: {
-                main: 'index.html',
-                greeting: 'greeting/greeting.html',
+                main: resolve(__dirname, 'index.html'),
+                greeting: resolve(__dirname, 'greeting.html'),
             },
         },
-    }
+    },
 });
